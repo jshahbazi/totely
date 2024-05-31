91a777a997b1b656c850250c277d898532007665
John Shahbazian <jshahbazi@gmail.com>
2024-05-30 23:40:25 -0700

    Debug CORS error

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ const App = () => {
     try {
       const r2 = new S3Client({
         region: "auto",
-        endpoint: `https://${process.env.REACT_APP_R2_ACCOUNT_ID}.r2.cloudflarestorage.com\totely`,
+        endpoint: `https://${process.env.REACT_APP_R2_ACCOUNT_ID}.r2.cloudflarestorage.com/${process.env.REACT_APP_R2_BUCKET_NAME}`,
         credentials: {
           accessKeyId: process.env.REACT_APP_R2_ACCESS_KEY_ID,
           secretAccessKey: process.env.REACT_APP_R2_SECRET_ACCESS_KEY,
