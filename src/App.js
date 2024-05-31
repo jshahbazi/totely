dee2df7c679b84347658af43c8e05f904614e153
John Shahbazian <jshahbazi@gmail.com>
2024-05-30 19:02:56 -0700

    Actually upload the file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { toast } from "react-toastify";
 // import Spinner from "./components/Spinner";
 // import ImageList from "./components/Images";
 
-// import axios from "axios";
+import axios from "axios";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { S3Client } from "@aws-sdk/client-s3";
