e80b01a1e8cdc42acdac3b60555778a28ce61cc9
John Shahbazian <jshahbazi@gmail.com>
2024-05-30 18:13:49 -0700

    Fix linter warning

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { toast } from "react-toastify";
 // import Spinner from "./components/Spinner";
 // import ImageList from "./components/Images";
 
-import axios from "axios";
+// import axios from "axios";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { S3Client } from "@aws-sdk/client-s3";
@@ -157,15 +157,15 @@ const App = () => {
     return signedUrl;
   }
 
-  async function addOrRetrieveFile(dataToSave) {
-    const options = {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const result = await axios.post("/write_to_D1", dataToSave, options);
-    return result.data;
-  }
+  // async function addOrRetrieveFile(dataToSave) {
+  //   const options = {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   };
+  //   const result = await axios.post("/write_to_D1", dataToSave, options);
+  //   return result.data;
+  // }
 
   async function handleFile(file, fileData) {
     // const { action, filePath } = await addOrRetrieveFile(fileData);
