f0621e1c4865ca2a24857e7bd828b9d3a7bfa94a
John Shahbazian <jshahbazi@gmail.com>
2024-05-31 14:59:27 -0700

    Fix bug

--- functions/get_file_details.js
+++ functions/get_file_details.js
@@ -8,7 +8,18 @@ export const onRequestGet = async ({ request, env, params }) => {
         return new Response(`File with id ${id} not found`, { status: 404 });
       } else {
         // Convert last_modified timestamp to ISO string
-        result.results[0].last_modified = new Date(result.results[0].last_modified).toISOString();
+        console.log(result.results[0].last_modified); // should log 1717138739000
+
+
+        const lastModified = Number(result.results[0].last_modified);
+        console.log(lastModified); // should log 1717138739000
+        const date = new Date(lastModified);
+        console.log(date); // should log a valid Date object
+        const isoString = date.toISOString();
+        console.log(isoString); // should log 2024-05-31T15:18:59.000Z
+                
+        result.results[0].last_modified = isoString;
+
         return new Response(JSON.stringify(result.results[0]), { status: 200 });
       }
     } catch (e) {
