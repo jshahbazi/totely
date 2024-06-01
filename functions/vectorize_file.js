8b2f06bf2b5f4f6b6254534d07b73a440b618eea
John Shahbazian <jshahbazi@gmail.com>
2024-06-01 15:20:08 -0700

    Vectorize files

--- functions/vectorize_file.js
+++ functions/vectorize_file.js
@@ -1,18 +1,36 @@
 import Replicate from 'replicate';
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const onRequestPost = async ({ request, env }) => {
   const replicate = new Replicate({
     auth: env.REPLICATE_API_TOKEN,
   });
 
-  const { filePath, bucket } = await request.json();
-  const accountId = env.CLOUDFLARE_ACCOUNT_ID;
+  const { filePath } = await request.json();
 
-  const input = {
-    input: `https://${accountId}.r2.cloudflarestorage.com/${bucket}/${filePath}`
-  };
+  const r2 = new S3Client({
+    region: "auto",
+    endpoint: `https://${env.REACT_APP_R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+      accessKeyId: env.REACT_APP_R2_ACCESS_KEY_ID,
+      secretAccessKey: env.REACT_APP_R2_SECRET_ACCESS_KEY,
+    },
+  });
 
-  const output = await replicate.run("daanelson/imagebind:0383f62e173dc821ec52663ed22a076d9c970549c209666ac3db181618b7a304", { input });
+  const getObjectCommand = new GetObjectCommand({
+    Bucket: env.REACT_APP_R2_BUCKET_NAME,
+    Key: filePath,
+  });
+
+  const signedUrl = await getSignedUrl(r2, getObjectCommand, { 
+    expiresIn: 60,
+    headers: {
+      'Content-Disposition': `attachment; filename="${filePath}"` // Ensure the correct filename is suggested
+    }
+  });  
+
+  const output = await replicate.run("daanelson/imagebind:0383f62e173dc821ec52663ed22a076d9c970549c209666ac3db181618b7a304", { signedUrl });
 
   return new Response(JSON.stringify({ vector: output }), {
     headers: { 'Content-Type': 'application/json' },
