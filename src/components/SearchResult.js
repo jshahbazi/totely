ae12bf81f135dcb706722664bbea203bb9d16446
John Shahbazian <jshahbazi@gmail.com>
2024-06-02 17:22:49 -0700

    Improve search results display

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -7,7 +7,10 @@ const SearchResult = ({ files, onFileClick }) => {
       {files.map(file => (
         <div key={file.id} className="search-result-item" onClick={() => onFileClick(file)}>
           <img src={file.signedUrl} alt={file.name} className="thumbnail" />
-          <div className="file-name">{file.name}</div>
+          <div className="file-info">
+            <div className="file-name">{file.name}</div>
+            <div className="file-score">Score: {file.score.toFixed(2)}</div>
+          </div>
         </div>
       ))}
     </div>
