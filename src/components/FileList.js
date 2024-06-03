6a8dc540f1a77eee9a3ce697124f35698c0d384b
John Shahbazian <jshahbazi@gmail.com>
2024-06-02 19:03:48 -0700

    Fix file detail

--- src/components/FileList.js
+++ src/components/FileList.js
@@ -5,8 +5,8 @@ const FileList = ({ files, onFileClick, onFileDelete, onFileDownload }) => {
   return (
     <FileListContainer>
       {files.map((file) => (
-        <FileItem key={file.id}>
-          <FileName onClick={() => onFileClick(file)}>{file.name}</FileName>
+        <FileItem key={file.id} onClick={() => onFileClick(file)}>
+          <FileName>{file.name}</FileName>
           <Button onClick={() => onFileDelete(file.id)}>Delete</Button>
           <Button onClick={() => onFileDownload(file)}>Download</Button>
         </FileItem>
