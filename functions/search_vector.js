0e6ea8c36e0d216c15dbccdc76d300e20f3ae8e7
John Shahbazian <jshahbazi@gmail.com>
2024-06-02 17:13:05 -0700

    Improve search results display

--- functions/search_vector.js
+++ functions/search_vector.js
@@ -10,7 +10,8 @@ export const onRequestPost = async ({ request, env }) => {
     const { query } = await request.json();
 
     const input = {
-        input: query
+        text_input: query,
+        modality: "text"
     };
 
     const vector_output = await replicate.run("daanelson/text-to-vector:0383f62e173dc821ec52663ed22a076d9c970549c209666ac3db181618b7a304", { input });
